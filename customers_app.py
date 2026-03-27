ec635d3a3e10ab00bdf44a858070d831b3c022ca
Damian Altamirano Ontiveros Rivero <daltamiranoontiveros@gmail.com>
2026-03-27 17:19:32 +0100

    Add SEARCH_ON column to fruit options display

--- customers_app.py
+++ customers_app.py
@@ -13,8 +13,9 @@ st.write(
 name_on_order = st.text_input('Name on Smoothie:')
 st.write("Name on order: "+name_on_order)
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-# st.dataframe(data=my_dataframe, width='stretch')
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
+st.dataframe(data=my_dataframe, width='stretch')
+st.stop()
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients: ',
     my_dataframe,
