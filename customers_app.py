bf30ad96071dea29c5b36fd0f43184c88da5fd4c
Damian Altamirano Ontiveros Rivero <daltamiranoontiveros@gmail.com>
2026-03-27 16:53:45 +0100

    Fetch and display nutrition info for selected fruits

--- customers_app.py
+++ customers_app.py
@@ -21,23 +21,17 @@ ingredients_list = st.multiselect(
     max_selections=5
 )
 
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
-st.text(smoothiefroot_response)
-
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width = True)
-
 if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-        
-    # st.write(ingredients_string)
-
+        st.subheader(fruit_chosen + ' Nutrition Information')
+        smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{fruit_chosen}")  
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), width='stretch')
     my_insert_stmt = """
         insert into smoothies.public.orders(ingredients,name_on_order)
         values ('""" + ingredients_string + """','""" + name_on_order +"""');
     """
-    # st.write(my_insert_stmt)
     time_to_insert = st.button('Submit Order')
     
     if time_to_insert:
