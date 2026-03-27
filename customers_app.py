6e54eb538f476b626e1460e341f2e721522ab42e
Damian Altamirano Ontiveros Rivero <daltamiranoontiveros@gmail.com>
2026-03-27 17:27:35 +0100

    Comment out dataframe display and stop
    Comment out dataframe display and stop statements.

--- customers_app.py
+++ customers_app.py
@@ -15,11 +15,10 @@ name_on_order = st.text_input('Name on Smoothie:')
 st.write("Name on order: "+name_on_order)
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-#st.dataframe(data=my_dataframe, width='stretch')
-#st.stop()
+
 pd_df=my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+#st.dataframe(pd_df)
+#st.stop()
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients: ',
     my_dataframe,
