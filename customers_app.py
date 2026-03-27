19c2e044d2841f57de6e5240015823bea0c94798
Damian Altamirano Ontiveros Rivero <daltamiranoontiveros@gmail.com>
2026-03-27 16:48:02 +0100

    Fix URL formatting in API request

--- customers_app.py
+++ customers_app.py
@@ -21,7 +21,7 @@ ingredients_list = st.multiselect(
     max_selections=5
 )
 
-smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
 st.text(smoothiefroot_response)
 
 sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width = True)
